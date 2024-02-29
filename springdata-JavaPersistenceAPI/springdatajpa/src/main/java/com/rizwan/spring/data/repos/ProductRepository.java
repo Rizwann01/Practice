db562b1696b8e6991161f1043668e69c19268980
Rizwann01 <120438005+Rizwann01@users.noreply.github.com>
2024-02-29 15:14:59 +0530

    Add files via upload
    Spring Boot-JPA-Project Added.

---
+++ springdata-JavaPersistenceAPI/springdatajpa/src/main/java/com/rizwan/spring/data/repos/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.rizwan.spring.data.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rizwan.spring.data.entities.Product;
+
+import java.util.List;
+
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+	
+	List<Product> findByPrice(Double price);
+	List<Product> findByName(String name);
+	
+	List<Product> findByNameAndPrice(String name, Double price);
+}
